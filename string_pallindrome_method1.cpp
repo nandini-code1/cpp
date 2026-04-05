9dfdf482684a65251963db222e4ff0e9822ae2e4
Nandini <119597562+nandini-code1@users.noreply.github.com>
2026-04-05 21:05:04 +0530

    Update string_pallindrome_method1.cpp

--- string_pallindrome_method1.cpp
+++ string_pallindrome_method1.cpp
@@ -1,7 +1,8 @@
 #include <iostream> 
+using namespace std;
 int main() { 
     //// method one
-    std::cout << "**Program for string pallindrome program ** \n"; 
+    std::cout << "**Program for string palindrome program ** \n"; 
     std::cout<<"Enter the string you need to check: "; 
     std::string str;
     std::cin>>str;
@@ -10,8 +11,8 @@ int main() {
         rev=rev+str[i];
     }
     if(str==rev){
-        std::cout<<str<<" is a pallindrome";
+        std::cout<<str<<" is a palindrome";
     }
-    else{std::cout<<str<<" is not a pallindrome";}
+    else{std::cout<<str<<" is not a palindrome";}
     return 0;
 }
